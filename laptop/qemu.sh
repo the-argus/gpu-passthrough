24bd8a9089b6c167b5ae19657e10b5339c63e66a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-31 10:47:15 -0400

    dont bother passing through audio

--- laptop/qemu.sh
+++ laptop/qemu.sh
@@ -27,15 +27,15 @@ else
         -cpu host,kvm=on,hv_relaxed,hv_spinlocks=0x1fff,hv_time,hv_vapic,hv_vendor_id=0xDEADBEEFFF \
         -bios /etc/ovmf/OVMF_CODE.fd \
         -device vfio-pci,host=$IOMMU_GPU,x-vga=on,romfile=$ROMFILE \
-        -device vfio-pci,host=$IOMMU_GPU_AUDIO \
-        -device vfio-pci,host=$IOMMU_AUDIO_SECONDARY \
-        -device vfio-pci,host="00:1f.4" \
-        -device vfio-pci,host="00:1f.5" \
         -usb \
         -device usb-host,hostbus=1,hostport=5 \
         -device usb-host,hostbus=1,hostport=6 \
         -drive file=$WINDOWS_IMG,media=disk,format=raw >> $LOG 2>&1
         # -device virtio-net-pci,netdev=n1 \
         # -netdev user,id=n1 \
+        # -device vfio-pci,host=$IOMMU_GPU_AUDIO \
+        # -device vfio-pci,host=$IOMMU_AUDIO_SECONDARY \
+        # -device vfio-pci,host="00:1f.4" \
+        # -device vfio-pci,host="00:1f.5" \
 fi
 
