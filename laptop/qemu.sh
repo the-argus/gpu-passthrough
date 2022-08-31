cf1bf9eeea16b8081580117a91de4fc002f401af
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-31 09:32:47 -0400

    install script

--- laptop/qemu.sh
+++ laptop/qemu.sh
@@ -3,7 +3,7 @@
 # useful for understanding usb passthrough options (by port, product, etc)
 # https://unix.stackexchange.com/questions/452934/can-i-pass-through-a-usb-port-via-qemu-command-line
 
-source config
+source ./config
 
 if [[ $1 == "debug" ]]; then
     echo "debugging..."
@@ -15,7 +15,7 @@ if [[ $1 == "debug" ]]; then
         -device virtio-net-pci,netdev=n1 \
         -netdev user,id=n1 \
         -rtc clock=host,base=localtime \
-        -bios /usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
+        -bios /etc/ovmf/OVMF_CODE.fd \
         -drive file=$WINDOWS_IMG,media=disk,format=raw >> $LOG 2>&1
 else
     qemu-system-x86_64 -runas $VM_USER \
@@ -25,7 +25,7 @@ else
         -smp $CORES \
         -nographic \
         -cpu host,kvm=on,hv_relaxed,hv_spinlocks=0x1fff,hv_time,hv_vapic,hv_vendor_id=0xDEADBEEFFF \
-        -bios /usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
+        -bios /etc/ovmf/OVMF_CODE.fd \
         -device vfio-pci,host=$IOMMU_GPU,x-vga=on,romfile=$ROMFILE \
         -device vfio-pci,host=$IOMMU_GPU_AUDIO \
         -usb \
