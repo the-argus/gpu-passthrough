9f7be0864e60d867da04ea076608a45f3305495f
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-31 11:01:10 -0400

    debug mode

--- laptop/qemu.sh
+++ laptop/qemu.sh
@@ -10,29 +10,33 @@ if [[ $1 == "debug" ]]; then
     qemu-system-x86_64 -runas $VM_USER \
         -enable-kvm \
         -m $RAM \
-        -smp $CORES \
-        -cpu host,kvm=on,hv_relaxed,hv_spinlocks=0x1fff,hv_time,hv_vapic,hv_vendor_id=0xDEADBEEFFF \
+        -rtc clock=host,base=localtime \
+        -smp $CORES,sockets=1,cores=$(( $CORES / 2 )),threads=2 \
+        -parallel none -serial none \
+        -cpu host,kvm=off,hv_relaxed,hv_spinlocks=0x1fff,hv_time,hv_vapic,hv_vendor_id=0xDEADBEEFFF \
+        -bios /etc/ovmf/OVMF_CODE.fd \
         -device virtio-net-pci,netdev=n1 \
         -netdev user,id=n1 \
-        -rtc clock=host,base=localtime \
-        -bios /etc/ovmf/OVMF_CODE.fd \
+        -usb \
+        -device usb-host,hostbus=1,hostport=5 \
+        -device usb-host,hostbus=1,hostport=6 \
         -drive file=$WINDOWS_IMG,media=disk,format=raw >> $LOG 2>&1
 else
     qemu-system-x86_64 -runas $VM_USER \
         -enable-kvm \
         -m $RAM \
         -rtc clock=host,base=localtime \
-        -smp $CORES \
-        -nographic \
-        -cpu host,kvm=on,hv_relaxed,hv_spinlocks=0x1fff,hv_time,hv_vapic,hv_vendor_id=0xDEADBEEFFF \
+        -smp $CORES,sockets=1,cores=$(( $CORES / 2 )),threads=2 \
+        -nographic -vga none -parallel none -serial none \
+        -cpu host,kvm=off,hv_relaxed,hv_spinlocks=0x1fff,hv_time,hv_vapic,hv_vendor_id=0xDEADBEEFFF \
         -bios /etc/ovmf/OVMF_CODE.fd \
-        -device vfio-pci,host=$IOMMU_GPU,x-vga=on,romfile=$ROMFILE \
+        -device virtio-net-pci,netdev=n1 \
+        -netdev user,id=n1 \
+        -device vfio-pci,host=$IOMMU_GPU,multifunction=on,x-vga=on \
         -usb \
         -device usb-host,hostbus=1,hostport=5 \
         -device usb-host,hostbus=1,hostport=6 \
         -drive file=$WINDOWS_IMG,media=disk,format=raw >> $LOG 2>&1
-        # -device virtio-net-pci,netdev=n1 \
-        # -netdev user,id=n1 \
         # -device vfio-pci,host=$IOMMU_GPU_AUDIO \
         # -device vfio-pci,host=$IOMMU_AUDIO_SECONDARY \
         # -device vfio-pci,host="00:1f.4" \
